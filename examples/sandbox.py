78be44ffdb564e5e8e6154dfc0529709436ad4f4
Samuel Colvin <s@muelcolvin.com>
2025-09-02 13:17:49 -0700

    improve sandbox example

--- examples/sandbox.py
+++ examples/sandbox.py
@@ -1,3 +1,6 @@
+import asyncio
+import time
+
 from mcp_run_python import code_sandbox
 
 
@@ -6,9 +9,10 @@ def log_handler(level: str, message: str):
 
 
 code = """
-import numpy
+import numpy, asyncio
 a = numpy.array([1, 2, 3])
 print(a)
+await asyncio.sleep(1)
 a
 """
 
@@ -16,12 +20,18 @@ a
 async def main():
     async with code_sandbox(dependencies=['numpy'], log_handler=log_handler, logging_level='debug') as sandbox:
         print('running code')
-        print(await sandbox.eval('print(1)'))
-        print(await sandbox.eval('print(2)'))
-        print(await sandbox.eval('print(3)'))
+        result = await sandbox.eval(code)
+        print(f'{result["status"].title()}:')
+        if result['status'] == 'success':
+            print(result['return_value'])
+        else:
+            print(result['error'])
+
+        tic = time.time()
+        result = await asyncio.gather(*[sandbox.eval(code) for _ in range(10)])
+        toc = time.time()
+        print(f'Execution time: {toc - tic:.3f} seconds')
 
 
 if __name__ == '__main__':
-    import asyncio
-
     asyncio.run(main())
