88e46f8a9ee8b824b760524b83f451cb2b41b99d
luoshasha <lssha@live.com>
2025-12-04 11:32:47 +0800

    style: fix code style.

--- mcp_run_python/_cli.py
+++ mcp_run_python/_cli.py
@@ -27,7 +27,9 @@ def cli_logic(args_list: Sequence[str] | None = None) -> int:
     )
     parser.add_argument('--port', type=int, help='Port to run the server on, default 3001.')
     parser.add_argument('--deps', '--dependencies', help='Comma separated list of dependencies to install')
-    parser.add_argument( '--disable-networking', action='store_true', help='Disable networking during execution of python code' )
+    parser.add_argument(
+        '--disable-networking', action='store_true', help='Disable networking during execution of python code'
+    )
     parser.add_argument('--verbose', action='store_true', help='Enable verbose logging')
     parser.add_argument('--version', action='store_true', help='Show version and exit')
     parser.add_argument(
